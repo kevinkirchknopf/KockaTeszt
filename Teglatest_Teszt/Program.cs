725e074ae44b277a0aa3a8d80000cecd5ef1908b
Kirchknopf Kevin <90893689+kevinkirchknopf@users.noreply.github.com>
2024-01-22 17:40:15 +0100

    Add files via upload

---
+++ Teglatest_Teszt/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Teglatest_Teszt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TeglaTest Tt = new TeglaTest();
+            Console.WriteLine("Add meg a test méreteit");
+            Console.WriteLine("A:");
+            
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("B:");
+
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("C:");
+            int C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Térfogat: {Tt.Terfogat(A,B,C)} \n Felszín: {Tt.Felszin(A,B,C)}, \ns Átló: {Math.Round(Tt.Atlo(A,B,C))}");
+            Console.ReadLine();
+        }
+
+     
+        
+    }
+}
---
+++ Teglatest_Teszt/TeglaTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teglatest_Teszt
+{
+    public class TeglaTest
+    {
+        private int A, B, C;
+
+        public int Terfogat(int A, int B, int C)
+        {
+            return A * B * C;
+        }
+
+        public int Felszin(int A, int B, int C) 
+        {
+            return 2 * (A * B + B * C + C * A);
+        }
+
+        public double Atlo(int A, int B, int C) 
+        {
+            return Math.Round(Math.Sqrt(A*A + B*B + C*C), 2);
+        }
+
+    }
+}
